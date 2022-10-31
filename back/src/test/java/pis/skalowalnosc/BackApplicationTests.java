33dc328a8c90073aff40750ef40aee9d373fb3c1
vamel <79980481+vamel@users.noreply.github.com>
2022-10-31 10:13:59 +0100

    PIS-3 added empty Java app with Spring

---
+++ back/src/test/java/pis/skalowalnosc/BackApplicationTests.java
@@ -0,0 +1,13 @@
+package pis.skalowalnosc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
