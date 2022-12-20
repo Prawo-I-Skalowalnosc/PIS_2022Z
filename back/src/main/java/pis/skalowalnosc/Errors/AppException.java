9778570dac554c62f1bd023b769250a9c9e3aa7f
HubSor <44928255+HubSor@users.noreply.github.com>
2022-12-20 20:44:55 +0100

    PIS-32. Dodanie error handlingu na backendzie.

---
+++ back/src/main/java/pis/skalowalnosc/Errors/AppException.java
@@ -0,0 +1,7 @@
+package pis.skalowalnosc.Errors;
+
+public class AppException extends Exception {
+    public AppException(String s) {
+        super(s);
+    }
+}
---
+++ back/src/main/java/pis/skalowalnosc/Errors/ErrorResponse.java
@@ -0,0 +1,21 @@
+package pis.skalowalnosc.Errors;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+
+public class ErrorResponse {
+    public HttpStatus status;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
+    public final LocalDateTime timestamp;
+    public String message;
+    public String infoMessage;
+
+    public ErrorResponse(HttpStatus status, String message, Throwable ex) {
+        timestamp = LocalDateTime.now();
+        this.status = status;
+        this.message = message; // don't show to user
+        this.infoMessage = ex.getLocalizedMessage(); // show to user
+    }
+}
