47ced35b0db3549e01a30fd2e98e8595c61f3920
HubSor <44928255+HubSor@users.noreply.github.com>
2022-12-20 20:46:13 +0100

    PIS-32. Usunięcie testowego kontrolera.

--- back/src/main/java/pis/skalowalnosc/controller/MovieController.java
+++ back/src/main/java/pis/skalowalnosc/controller/MovieController.java
@@ -3,17 +3,27 @@ package pis.skalowalnosc.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pis.skalowalnosc.model.Movie;
 import pis.skalowalnosc.service.MovieService;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
+@RequestMapping("/movies")
 public class MovieController {
     @Autowired
     private MovieService movieService;
 
     @GetMapping("/first")
-    public String firstTitle() {
+    public String first() {
         return movieService.findAll().get(0).getTitle();
+    }
+
+    @GetMapping("/all")
+    public List<Movie> all() {
+        return movieService.findAll();
     }
 }
