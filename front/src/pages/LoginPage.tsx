b58e7b87d3fb55bc9bdc20fc09817512eb138289
HubSor <44928255+HubSor@users.noreply.github.com>
2023-01-01 13:39:17 +0100

    PIS-34. Dodanie zapamiętywania tokena na froncie.

--- front/src/pages/LoginPage.tsx
+++ front/src/pages/LoginPage.tsx
@@ -4,6 +4,7 @@ import {Link, useNavigate} from "react-router-dom";
 import { LoginForm } from "../components/LoginForm";
 import {ErrorAndInfo} from "../components/ErrorAndInfo";
 import Layout from "../components/layout/Layout";
+import {TokenHelper} from "../helpers/TokenHelper";
 
 export default function LoginPage() {
     const [error, setError] = useState("");
@@ -16,7 +17,10 @@ export default function LoginPage() {
                     <div className="pis-login-error">
                         <ErrorAndInfo errorMsg={error} infoMsg={""}/>
                     </div>
-                    <LoginForm onSuccess={() => navigate("/")} onError={(res) => setError(res.infoMessage)}/>
+                    <LoginForm onSuccess={(res) => {
+                        TokenHelper.setToken(res.token)
+                        navigate("/")
+                    }} onError={(res) => setError(res.infoMessage)}/>
                     <small className="pis-login-reglink-text">
                         Nie masz konta?
                         <Link className="pis-link pis-login-reglink" to="/register"> Załóż konto</Link>
