9f8dbdbb0356e442cf97907d2d287e1bc031290d
vamel <79980481+vamel@users.noreply.github.com>
2023-01-07 01:08:19 +0100

    przycisk dodania filmu może byc nieaktywny jeżeli obowiązkowe pola nie są uzupełnione

--- front/src/components/NewMovieForm.tsx
+++ front/src/components/NewMovieForm.tsx
@@ -1,4 +1,5 @@
 import {useState} from "react";
+import { useNavigate } from "react-router-dom";
 import {Helmet} from "react-helmet";
 import {Button, Card, Grid, MenuItem, TextField} from "@mui/material";
 import Stack from "@mui/material/Stack";
@@ -11,7 +12,19 @@ import SendIcon from '@mui/icons-material/Send'
 import "../style/register.css"
 
 export function NewMovieForm() {
-    const [movieData, setMovieData] = useState({} as MovieResponse);
+    const defaultDate = new Date().toISOString().split('T')[0];
+    const [movieData, setMovieData] = useState({release_date: defaultDate} as MovieResponse);
+    let navigate = useNavigate();
+
+    const handleInput = () => {
+        setMovieData({...movieData, rating: 0});
+        console.log(movieData);
+    }
+
+    const goToMainPage = () => {
+        const path="/";
+        navigate(path);
+    }
 
     return (
         <>
@@ -25,51 +38,85 @@ export function NewMovieForm() {
                             Dodaj nowy film
                         <FormControl className={"pis-newmovie-form"}>
                             <TextField
+                                required
                                 label={"Tytuł filmu"}
-                                required
-                                sx={{mt: "1rem"}}/>
+                                name={"title"}
+                                sx={{mt: "1rem"}}
+                                onChange={(e) =>
+                                        setMovieData({...movieData, title: e.target.value})}/>
                             <TextField
                                 required
                                 select
+                                name={"genre"}
                                 label={"Gatunek"}
-                                sx={{mt: "1rem"}}>
+                                sx={{mt: "1rem"}}
+                                defaultValue={''}
+                                onChange={(e) =>
+                                    setMovieData({...movieData, genre: e.target.value})}>
                                 {GENRES.map((genre: string) => {
-                                    return <MenuItem value={genre}>{genre}</MenuItem>
+                                    return <MenuItem key={genre} value={genre}>{genre}</MenuItem>
                                 })}
                             </TextField>
                             <TextField
                                 select
+                                name={"country_of_origin"}
                                 label={"Kraj powstania"}
-                                sx={{mt: "1rem"}}>
+                                sx={{mt: "1rem"}}
+                                defaultValue={''}
+                                onChange={(e) =>
+                                    setMovieData({...movieData, country_of_origin: e.target.value})}>
                                 {COUNTRIES.map((country: string) => {
-                                    return <MenuItem value={country}>{country}</MenuItem>
+                                    return <MenuItem key={country} value={country}>{country}</MenuItem>
                                 })}
                             </TextField>
                             <TextField
+                                name={"budget"}
                                 type={"number"}
-                                label={"Budżet (w mln $)"}
-                                sx={{mt: "1rem"}}/>
+                                label={"Budżet ($)"}
+                                sx={{mt: "1rem"}}
+                                onChange={(e) =>
+                                    setMovieData({...movieData, budget: parseInt(e.target.value, 10)})}/>
                             <TextField
+                                name={"language"}
                                 label={"Język filmu"}
-                                sx={{mt: "1rem"}}/>
+                                sx={{mt: "1rem"}}
+                                onChange={(e) =>
+                                    setMovieData({...movieData, language: e.target.value})}/>
                             <TextField
+                                name={"release_date"}
                                 type={"date"}
                                 label={"Data premiery"}
-                                defaultValue={new Date().toISOString().split('T')[0]}
-                                sx={{mt: "1rem"}}/>
+                                defaultValue={defaultDate}
+                                sx={{mt: "1rem"}}
+                                onChange={(e) =>
+                                    setMovieData({...movieData, release_date: e.target.value})}/>
                             <TextField
+                                name={"length"}
                                 type={"number"}
                                 label={"Długość trwania filmu (w minutach)"}
-                                sx={{mt: "1rem"}}/>
-                        </FormControl>
-                            <Grid sx={{mt: "1rem"}}>
-                                <Button variant="contained" color="error" sx={{m: "0 10px"}}>
-                                    Cancel
+                                sx={{mt: "1rem"}}
+                                onChange={(e) =>
+                                    setMovieData({...movieData, length: parseInt(e.target.value, 10)})}/>
+                            <Grid sx={{mt: "1rem", textAlign: "center"}}>
+                                <Button
+                                    type="button"
+                                    variant="contained"
+                                    onClick={goToMainPage}
+                                    color="error"
+                                    sx={{m: "0 10px"}}>
+                                    Powróć
                                 </Button>
-                                <Button variant="contained" endIcon={<SendIcon />} sx={{m: "0 10px"}}>
-                                    Add movie
+                                <Button
+                                    type="submit"
+                                    variant="contained"
+                                    onClick={handleInput}
+                                    endIcon={<SendIcon />}
+                                    sx={{m: "0 10px"}}
+                                    disabled={!movieData.title || !movieData.genre}>
+                                    Dodaj film
                                 </Button>
                             </Grid>
+                        </FormControl>
                         </Stack>
                 </Card>
                 </main>
