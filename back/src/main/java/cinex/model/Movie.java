7a9764fff430dd3b6d3a40c498c619648a94046c
vamel <79980481+vamel@users.noreply.github.com>
2023-01-07 23:58:36 +0100

    PIS-22 zmieniono konstruktor Movie(CreateMovieRequest) aby dodawał budżet

--- back/src/main/java/cinex/model/Movie.java
+++ back/src/main/java/cinex/model/Movie.java
@@ -46,6 +46,7 @@ public class Movie {
         rating = request.rating;
         genre = request.genre;
         country_of_origin = request.country_of_origin;
+        budget = request.budget;
         language = request.language;
         releaseDate = request.releaseDate != null ? request.releaseDate : new Date();
         length = request.length;
