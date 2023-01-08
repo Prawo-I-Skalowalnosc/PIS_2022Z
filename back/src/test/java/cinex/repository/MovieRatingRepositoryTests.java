06c665e528ff150258516f9cc13e54592bc4ac04
HubSor <44928255+HubSor@users.noreply.github.com>
2023-01-08 12:12:13 +0100

    PIS-20. Poprawki testów.

--- back/src/test/java/cinex/repository/MovieRatingRepositoryTests.java
+++ back/src/test/java/cinex/repository/MovieRatingRepositoryTests.java
@@ -3,15 +3,20 @@ package cinex.repository;
 import cinex.model.Movie;
 import cinex.model.MovieRating;
 import cinex.model.User;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.AfterEach;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
 import java.util.Optional;
 
 import static cinex.GlobalTestValues.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@SpringBootTest
 public class MovieRatingRepositoryTests {
     @Autowired
     private MovieRatingRepository movieRatingRepository;
@@ -21,33 +26,37 @@ public class MovieRatingRepositoryTests {
     private MovieRepository movieRepository;
 
     @AfterEach
+    @BeforeEach
     public void clean() {
-        Movie movie = getMovie();
-        User user = getUser();
-        Optional<MovieRating> movieRatings;
-        movieRatings = movieRatingRepository.findByRaterAndMovie(user, movie);
-        movieRatings.ifPresent(movieRating -> movieRatingRepository.delete(movieRating));
-        movieRepository.delete(movie);
-        userRepository.delete(user);
+        Optional<User> user;
+        if ((user = userRepository.findByUsername(username)).isPresent()) {
+            var ratings = movieRatingRepository.findByRater(user.get());
+            movieRatingRepository.deleteAll(ratings);
+            userRepository.delete(user.get());
+        }
+        List<Movie> movies;
+        if (!(movies = movieRepository.findByTitle(title)).isEmpty())
+            movieRepository.deleteAll(movies);
+
     }
     private static void compareMovieRatings(MovieRating movieRating, MovieRating DBmovieRating) {
-        assertEquals(movieRating.getMovie(), DBmovieRating.getMovie());
-        assertEquals(movieRating.getRater(), DBmovieRating.getRater());
+        assertEquals(movieRating.getMovie().getId(), DBmovieRating.getMovie().getId());
+        assertEquals(movieRating.getRater().getId(), DBmovieRating.getRater().getId());
         assertEquals(movieRating.getValue(), DBmovieRating.getValue());
-        assertEquals(movieRating.getDate_rated(), DBmovieRating.getDate_rated());
     }
 
     @Test
     public void testFindByRaterAndMovie(){
         var movie = getMovie();
         var rater = getUser();
+        movie = movieRepository.save(movie);
+        rater = userRepository.save(rater);
+
         var rating = getMovieRating(movie, rater, 3);
-        movieRepository.save(movie);
         movieRatingRepository.save(rating);
-        userRepository.save(rater);
 
         var ratingDB = movieRatingRepository.findByRaterAndMovie(rater, movie);
+        assertTrue(ratingDB.isPresent());
         ratingDB.ifPresent(movieRating -> compareMovieRatings(rating, movieRating));
-
     }
 }
