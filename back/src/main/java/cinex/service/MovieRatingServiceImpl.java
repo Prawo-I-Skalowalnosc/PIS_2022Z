31a91e84bc9d884977e95adb5b7bfc9a08a9a426
HubSor <44928255+HubSor@users.noreply.github.com>
2023-01-08 23:48:13 +0100

    PIS-33. Poprawka z dodawaniem przedpremierowych ocen.

--- back/src/main/java/cinex/service/MovieRatingServiceImpl.java
+++ back/src/main/java/cinex/service/MovieRatingServiceImpl.java
@@ -1,6 +1,7 @@
 package cinex.service;
 
 import cinex.controller.api.responses.RatingResponse;
+import cinex.errors.AppException;
 import cinex.model.Movie;
 import cinex.model.MovieRating;
 import cinex.model.User;
@@ -8,6 +9,7 @@ import cinex.repository.MovieRatingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.Optional;
 
 @Service
@@ -19,13 +21,17 @@ public class MovieRatingServiceImpl implements MovieRatingService{
         return movieRatingRepository.findByRaterAndMovie(rater, movie);
     }
     @Override
-    public RatingResponse updateOrCreateRating(MovieRating movie_rating){
+    public RatingResponse updateOrCreateRating(MovieRating movie_rating) throws AppException {
+        if (movie_rating.getMovie().getReleaseDate().after(new Date()))
+            throw new AppException("Nie możesz ocenić filmu przed premierą");
         var isNew = getRating(movie_rating.getRater(), movie_rating.getMovie()).isEmpty();
         return new RatingResponse(movieRatingRepository.save(movie_rating), isNew);
     }
 
     @Override
-    public MovieRating create(Movie movie, User user, Integer rating){
+    public MovieRating create(Movie movie, User user, Integer rating) throws AppException {
+        if (movie.getReleaseDate().after(new Date()))
+            throw new AppException("Nie możesz ocenić filmu przed premierą");
         return movieRatingRepository.save(new MovieRating(movie, user, rating));
     }
 }
